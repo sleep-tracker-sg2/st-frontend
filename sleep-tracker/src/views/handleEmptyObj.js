117ca0f5fdfc3ee18b61be3cc67b6ca0980c8aa0
gina-christians <christiansgina@gmail.com>
2019-07-11 22:02:20 -0600

    changed handleRedux file to handleEmptyObject.js

---
+++ sleep-tracker/src/views/handleEmptyObj.js
@@ -0,0 +1,7 @@
+const ifEmpty = value =>
+  value === undefined ||
+  value == null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+export default ifEmpty;
